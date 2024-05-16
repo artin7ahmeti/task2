34adba1a4213efd980ea09890cde8401d57e0d0b
Artin Ahmeti <artin.ahmeti10@gmail.com>
2024-05-16 03:09:44 +0200

    build: changed some images, made the text centered and splattered the images across it.

--- task2.js
+++ task2.js
@@ -1,21 +1,19 @@
-$(window).bind('scroll',function(e){
-    parallaxScroll();
+$(window).bind('scroll', function(e){
+   parallaxScroll();
 });
 
 function parallaxScroll(){
-   var scrolled = $(window).scrollTop(); 
-   $('.layer-1').css('top',(0-(scrolled*.25))+'px');
-   $('.layer-2').css('top',(0-(scrolled*.5))+'px');
-   $('.layer-3').css('top',(0-(scrolled*.75))+'px');
-   $('.rock-1').css('top',(400-(scrolled*.8))+'px');
-   $('.rock-2').css('top',(200-(scrolled*.6))+'px');
-   $('.rock-3').css('top',(500-(scrolled*.4))+'px'); 
-   $('.rock-4').css('top',(600-(scrolled*.5))+'px'); 
-   $('.rock-5').css('top',(600-(scrolled*.7))+'px'); 
-   $('.rock-6').css('top',(400-(scrolled*.7))+'px'); 
-   $('.rock-7').css('top',(600-(scrolled*.5))+'px'); 
-   $('.rock-8').css('top',(200-(scrolled*.2))+'px');
-   $('.rock-9').css('top',(200-(scrolled*.4))+'px');
+   var scrolled = $(window).scrollTop();
+   $('.layer-1').css('top', (0 - (scrolled * .25)) + 'px');
+   $('.layer-2').css('top', (0 - (scrolled * .5)) + 'px');
+   $('.layer-3').css('top', (0 - (scrolled * .75)) + 'px');
+   $('.flower-1').css('top', (100 - (scrolled * .3)) + 'px');
+   $('.flower-2').css('top', (150 - (scrolled * .4)) + 'px');
+   $('.flower-3').css('top', (250 - (scrolled * .5)) + 'px');
+   $('.flower-4').css('top', (300 - (scrolled * .6)) + 'px');
+   $('.flower-5').css('top', (500 - (scrolled * .7)) + 'px');
+   $('.flower-6').css('top', (600 - (scrolled * .8)) + 'px');
+   $('.flower-7').css('top', (700 - (scrolled * .9)) + 'px');
+   $('.flower-8').css('top', (800 - (scrolled * 1)) + 'px');
+   $('.flower-9').css('top', (900 - (scrolled * 1.1)) + 'px');
 }
-
-
